542800c664feb4ec82edde6acc48cd133fb408ff
Soteded <theo.vicente@ynov.com>
2019-11-12 09:29:54 +0100

    Divers
    FavIcon + Titre

--- LaReponseD/resources/views/layouts/app.blade.php
+++ LaReponseD/resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="icon" type="image/png" href="../../../public/img/aled.jpg" />
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
