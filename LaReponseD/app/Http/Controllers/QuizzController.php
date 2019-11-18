cd79c4cf25fbffe9b068bc217bdd79df3ce7c87f
pierre salmi <pierre.salmi@ynov.com>
2019-11-18 14:27:59 +0100

    Quizz

---
+++ LaReponseD/app/Http/Controllers/QuizzController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Quizz;
+use Illuminate\Http\Request;
+
+class QuizzController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Quizz  $quizz
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Quizz $quizz)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Quizz  $quizz
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Quizz $quizz)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Quizz  $quizz
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Quizz $quizz)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Quizz  $quizz
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Quizz $quizz)
+    {
+        //
+    }
+}
---
+++ LaReponseD/database/factories/QuizzFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Quizz;
+use Faker\Generator as Faker;
+
+$factory->define(Quizz::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
