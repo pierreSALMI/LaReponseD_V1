c147cbd58615d98e7acbf0602ecf66ae136fd6be
Soteded <theo.vicente@ynov.com>
2019-11-25 13:49:58 +0100

    quiz

--- LaReponseD/app/Http/Controllers/QuizController.php
+++ LaReponseD/app/Http/Controllers/QuizController.php
@@ -49,10 +49,8 @@ class QuizController extends Controller
      */
     public function show($id)
     {
-        $questions = Question::where('quiz_id', $id)->get();
-
         $quiz = Quiz::with('questions')->find($id);
-        return view('quizBlade.show', ['quiz' => $quiz], compact('questions'));
+        return view('quizBlade.show', ['quiz' => $quiz]);
     }
 
     public function show2($quiz_id) {
