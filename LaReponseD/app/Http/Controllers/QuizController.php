477d246399d8f64e667289c65ee363b44f205e70
pierre salmi <pierre.salmi@ynov.com>
2019-12-02 15:04:35 +0100

    a

--- LaReponseD/app/Http/Controllers/QuizController.php
+++ LaReponseD/app/Http/Controllers/QuizController.php
@@ -94,7 +94,6 @@ class QuizController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $quiz = Quiz::with('questions.choix')->find($id);
         $request->validate([
             'theme'=>'required|string',
             'questions'=> ['required', 'array', function($attribute, $values, $fail) use($id){
