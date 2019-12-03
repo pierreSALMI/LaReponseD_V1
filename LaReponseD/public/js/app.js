1a5cc7fd741208a4a50e9b38929b17c731920a7f
Soteded <theo.vicente@ynov.com>
2019-12-03 13:09:16 +0100

    Sidebar
    Sidebar created

--- LaReponseD/public/js/app.js
+++ LaReponseD/public/js/app.js
@@ -49499,15 +49499,27 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/sidebar.scss":
+/*!*************************************!*\
+  !*** ./resources/sass/sidebar.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*******************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/sidebar.scss ***!
+  \*******************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\Users\User\Documents\Ynov\B2\WebDev\PHP\ProjetQuizz\LaReponseD\LaReponseD\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\User\Documents\Ynov\B2\WebDev\PHP\ProjetQuizz\LaReponseD\LaReponseD\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\User\Documents\Ynov\B2\WebDev\PHP\ProjetQuizz\LaReponseD\LaReponseD\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\User\Documents\Ynov\B2\WebDev\PHP\ProjetQuizz\LaReponseD\LaReponseD\resources\sass\sidebar.scss */"./resources/sass/sidebar.scss");
 
 
 /***/ })
